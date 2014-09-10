7bebdbdc1c9ff1a91f191e0822cb9d33c437c7c0
Joshua W <jaxxa90@gmail.com>
2014-09-10 21:19:36 +1000

    Not working, but backing up what I have done.

---
+++ Source/Shields/Pawns/ShieldedPawn.cs
@@ -0,0 +1,23 @@
+﻿using RimWorld;
+using RimWorld.SquadAI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse.AI;
+using Verse;
+
+namespace Jaxxa_Shields
+{
+    public class ShieldedPawn : Verse.Pawn
+    {
+        /*
+        protected override void ApplyDamage(Verse.DamageInfo dinfo)
+        {
+            Log.Error("Shield Took Hit");
+            base.ApplyDamage(dinfo);
+        }*/
+    }
+}
