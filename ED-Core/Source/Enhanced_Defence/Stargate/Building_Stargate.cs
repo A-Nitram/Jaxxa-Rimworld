8f9118b68d3270f08a19c76c2c5a1cb158f73c65
Jaxxa <jaxxa90@gmail.com>
2014-10-28 17:07:43 +1100

    Stargate pawn support (Apparently also a Cloning Bay Now)

--- ED-Core/Source/Enhanced_Defence/Stargate/Building_Stargate.cs
+++ ED-Core/Source/Enhanced_Defence/Stargate/Building_Stargate.cs
@@ -30,6 +30,28 @@ namespace Enhanced_Defence.Stargate
 
         }
 
+        //Saving game
+        public override void ExposeData()
+        {
+
+            Log.Message("Expose Data start");
+            //base.ExposeData();
+
+
+            //Scribe_Deep.LookDeep(ref listOfThingLists, "listOfThingLists");
+
+            /*Scribe_Values.LookValue<int>(ref DropPodAddUnitRadius, "DropPodAddUnitRadius");
+            Scribe_Values.LookValue<bool>(ref DropPodDeepStrike, "DropPodDeepStrike");
+            Scribe_Values.LookValue<bool>(ref DropPodAddUnits, "DropPodAddUnits");
+            Scribe_Values.LookValue<bool>(ref DropPodAddResources, "DropPodAddResources");*/
+
+            Log.Message("Expose Data - look list");
+            //Scribe_Collections.LookList<Thing>(ref listOfThingLists, "listOfThingLists", LookMode.Deep, (object)null);
+            //Scribe_Collections.LookList<Thing>(ref listOfOffworldThings, "listOfOffworldThings", LookMode.Deep, (object)null);
+
+            Log.Message("Expose Data about to start");
+
+        }
 
         public override IEnumerable<Command> GetCommands()
         {
@@ -41,84 +63,84 @@ namespace Enhanced_Defence.Stargate
                 CommandList = baseCommands.ToList();
             }
 
-            if (this.StargateAddResources)
+            if (true)
             {
                 //Upgrading
-                Command_Action command_Action_InstallShield = new Command_Action();
+                Command_Action command_Action_AddResources = new Command_Action();
 
-                command_Action_InstallShield.defaultLabel = "Add Resources";
+                command_Action_AddResources.defaultLabel = "Add Resources";
 
-                command_Action_InstallShield.icon = UI_ADD_RESOURCES;
-                command_Action_InstallShield.defaultDesc = "Add Resources";
+                command_Action_AddResources.icon = UI_ADD_RESOURCES;
+                command_Action_AddResources.defaultDesc = "Add Resources";
 
-                command_Action_InstallShield.activateSound = SoundDef.Named("Click");
-                command_Action_InstallShield.action = new Action(this.AddResources);
+                command_Action_AddResources.activateSound = SoundDef.Named("Click");
+                command_Action_AddResources.action = new Action(this.AddResources);
 
-                CommandList.Add(command_Action_InstallShield);
+                CommandList.Add(command_Action_AddResources);
             }
 
-            if (this.StargateAddUnits)
+            if (true)
             {
                 //Upgrading
-                Command_Action command_Action_InstallShield = new Command_Action();
+                Command_Action command_Action_AddColonist = new Command_Action();
 
-                command_Action_InstallShield.defaultLabel = "Add Colonist";
+                command_Action_AddColonist.defaultLabel = "Add Colonist";
 
-                command_Action_InstallShield.icon = UI_ADD_COLONIST;
-                command_Action_InstallShield.defaultDesc = "Add Colonist";
+                command_Action_AddColonist.icon = UI_ADD_COLONIST;
+                command_Action_AddColonist.defaultDesc = "Add Colonist";
 
-                command_Action_InstallShield.activateSound = SoundDef.Named("Click");
-                //command_Action_InstallShield.action = new Action(this.AddColonist);
+                command_Action_AddColonist.activateSound = SoundDef.Named("Click");
+                command_Action_AddColonist.action = new Action(this.AddColonist);
 
-                CommandList.Add(command_Action_InstallShield);
+                CommandList.Add(command_Action_AddColonist);
             }
-
+            /*
             if (this.StargateRetreave)
             {
                 //Upgrading
-                Command_Action command_Action_InstallShield = new Command_Action();
+                Command_Action command_Action_IncomingWormehole = new Command_Action();
 
-                command_Action_InstallShield.defaultLabel = "DeepStrike";
+                command_Action_IncomingWormehole.defaultLabel = "IncomingWormehole";
 
-                command_Action_InstallShield.icon = UI_DROPPOD;
-                command_Action_InstallShield.defaultDesc = "DeepStrike";
+                command_Action_IncomingWormehole.icon = UI_DROPPOD;
+                command_Action_IncomingWormehole.defaultDesc = "IncomingWormehole";
 
-                command_Action_InstallShield.activateSound = SoundDef.Named("Click");
+                command_Action_IncomingWormehole.activateSound = SoundDef.Named("Click");
                 //command_Action_InstallShield.action = new Action(this.DeepStrike);
 
-                CommandList.Add(command_Action_InstallShield);
+                CommandList.Add(command_Action_IncomingWormehole);
+            }*/
+
+            if (true)
+            {
+                //Upgrading
+                Command_Action command_Action_DialOut = new Command_Action();
+
+                command_Action_DialOut.defaultLabel = "Dial Out";
+
+                command_Action_DialOut.icon = UI_DROPPOD;
+                command_Action_DialOut.defaultDesc = "Dial Out";
+
+                command_Action_DialOut.activateSound = SoundDef.Named("Click");
+                command_Action_DialOut.action = new Action(this.StargateDialOut);
+
+                CommandList.Add(command_Action_DialOut);
             }
 
             if (true)
             {
                 //Upgrading
-                Command_Action command_Action_InstallShield = new Command_Action();
+                Command_Action command_Action_IncomingWormhole = new Command_Action();
 
-                command_Action_InstallShield.defaultLabel = "Dial Out";
+                command_Action_IncomingWormhole.defaultLabel = "Incoming WormHole";
 
-                command_Action_InstallShield.icon = UI_DROPPOD;
-                command_Action_InstallShield.defaultDesc = "Dial Out";
+                command_Action_IncomingWormhole.icon = UI_DROPPOD;
+                command_Action_IncomingWormhole.defaultDesc = "Incoming WormHole";
 
-                command_Action_InstallShield.activateSound = SoundDef.Named("Click");
-                command_Action_InstallShield.action = new Action(this.StargateDialOut);
+                command_Action_IncomingWormhole.activateSound = SoundDef.Named("Click");
+                command_Action_IncomingWormhole.action = new Action(this.StargateIncomingWormhole);
 
-                CommandList.Add(command_Action_InstallShield);
-            }
-
-            if (true)
-            {
-                //Upgrading
-                Command_Action command_Action_InstallShield = new Command_Action();
-
-                command_Action_InstallShield.defaultLabel = "Incoming WormHole";
-
-                command_Action_InstallShield.icon = UI_DROPPOD;
-                command_Action_InstallShield.defaultDesc = "Incoming WormHole";
-
-                command_Action_InstallShield.activateSound = SoundDef.Named("Click");
-                command_Action_InstallShield.action = new Action(this.StargateIncomingWormHole);
-
-                CommandList.Add(command_Action_InstallShield);
+                CommandList.Add(command_Action_IncomingWormhole);
             }
 
 
@@ -164,7 +186,7 @@ namespace Enhanced_Defence.Stargate
             Enhanced_Defence.Stargate.Saving.SaveThings.save(listOfOffworldThings, @"C:\Stargate.txt", this);
         }
 
-        public void StargateIncomingWormHole()
+        public void StargateIncomingWormhole()
         {
             //listOfOffworldThings.Clear();
 
@@ -180,27 +202,36 @@ namespace Enhanced_Defence.Stargate
             Log.Message("End of Placing");
         }
 
-        //Saving game
-        public override void ExposeData()
+        public void AddColonist()
         {
+            if (true)
+                //if (power.PowerOn)
+            {
+                //Log.Message("CLick AddColonist");
+                IEnumerable<Pawn> closePawns = Enhanced_Defence.Utilities.Utilities.findPawns(this.Position, 5);
 
-            Log.Message("Expose Data start");
-            //base.ExposeData();
+                if (closePawns != null)
+                {
+                    foreach (Pawn currentPawn in closePawns)
+                    {
+                        if (currentPawn.SpawnedInWorld)
+                        {
 
+                            List<Thing> thingList = new List<Thing>();
+                            //thingList.Add(foundThing);
+                            listOfOffworldThings.Add(currentPawn);
+                            currentPawn.DeSpawn();
 
-            //Scribe_Deep.LookDeep(ref listOfThingLists, "listOfThingLists");
-
-            /*Scribe_Values.LookValue<int>(ref DropPodAddUnitRadius, "DropPodAddUnitRadius");
-            Scribe_Values.LookValue<bool>(ref DropPodDeepStrike, "DropPodDeepStrike");
-            Scribe_Values.LookValue<bool>(ref DropPodAddUnits, "DropPodAddUnits");
-            Scribe_Values.LookValue<bool>(ref DropPodAddResources, "DropPodAddResources");*/
-
-            Log.Message("Expose Data - look list");
-            //Scribe_Collections.LookList<Thing>(ref listOfThingLists, "listOfThingLists", LookMode.Deep, (object)null);
-            //Scribe_Collections.LookList<Thing>(ref listOfOffworldThings, "listOfOffworldThings", LookMode.Deep, (object)null);
-
-            Log.Message("Expose Data about to start");
-
+                            //Building_OrbitalRelay.listOfThings.Add(currentPawn);
+                            //currentPawn.DeSpawn();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Messages.Message("Insufficient Power to add Colonist");
+            }
         }
     }
 }
