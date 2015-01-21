96de05168553d86299e06d1dfa3a9f183858c7d2
Joshua W <jaxxa90@gmail.com>
2015-01-21 21:51:08 +1100

    Trying to fix vents

---
+++ ED-Core/Source/Enhanced_Defence/Temperature/Building_ReverseCycleCooler.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace Enhanced_Defence.Temperature
+{
+    public class Building_ReverseCycleCooler : Building_Cooler
+    {
+        private static Texture2D UI_ADD_RESOURCES;
+        
+        public override void SpawnSetup()
+        {
+            base.SpawnSetup();
+            UI_ADD_RESOURCES = ContentFinder<Texture2D>.Get("UI/ADD_RESOURCES", true);
+        }
+
+       /* public override void TickRare()
+        {
+            if (!this.compPowerTrader.PowerOn)
+            {
+                return;
+            }
+            IntVec3 intVec3_1 = this.Position + Gen.RotatedBy(IntVec3.south, this.Rotation);
+            IntVec3 intVec3_2 = this.Position + Gen.RotatedBy(IntVec3.north, this.Rotation);
+
+        }*/
+
+        public override IEnumerable<Command> GetCommands()
+        {
+            IList<Command> CommandList = new List<Command>();
+            IEnumerable<Command> baseCommands = base.GetCommands();
+
+            if (baseCommands != null)
+            {
+                CommandList = baseCommands.ToList();
+            }
+
+            if (true)
+            {
+                //Upgrading
+                Command_Action command_Action_AddResources = new Command_Action();
+
+                command_Action_AddResources.defaultLabel = "Rotate";
+
+                command_Action_AddResources.icon = UI_ADD_RESOURCES;
+                command_Action_AddResources.defaultDesc = "Rotate";
+
+                command_Action_AddResources.activateSound = SoundDef.Named("Click");
+                command_Action_AddResources.action = new Action(this.ChangeRotation);
+
+                CommandList.Add(command_Action_AddResources);
+            }
+
+
+            return CommandList.AsEnumerable<Command>();
+            //return compPowerTrader.CompGetCommandsExtra();
+            //return base.GetCommands();
+        }
+
+        public void ChangeRotation()
+        {
+            //Log.Error("Rotation");
+
+            if (this.Rotation.AsInt == 3)
+            {
+                this.Rotation = new IntRot(0);
+            }
+            else
+            {
+
+                this.Rotation = new IntRot(this.Rotation.AsInt + 1);
+            }
+
+
+            // Tell the MapDrawer that here is something thats changed
+            Find.MapDrawer.MapChanged(Position, MapChangeType.Things, true, false);
+            
+            //this.Rotation.Rotate(RotationDirection.Clockwise);
+            //this.Rotation.Rotate(RotationDirection.Clockwise);
+
+        }
+
+    }
+}
--- ED-Core/Source/Enhanced_Defence/Temperature/Building_Vent.cs
+++ ED-Core/Source/Enhanced_Defence/Temperature/Building_Vent.cs
@@ -13,7 +13,7 @@ namespace Enhanced_Defence.Temperature
 
         public CompTempControl compTempControl;
         public CompPowerTrader compPowerTrader;
-        
+
         public override void SpawnSetup()
         {
             base.SpawnSetup();
@@ -22,6 +22,82 @@ namespace Enhanced_Defence.Temperature
         }
 
         public override void TickRare()
+        {
+            if (!this.compPowerTrader.PowerOn)
+            {
+                return;
+            }
+            IntVec3 intVec3_1 = this.Position + Gen.RotatedBy(IntVec3.south, this.Rotation);
+            IntVec3 intVec3_2 = this.Position + Gen.RotatedBy(IntVec3.north, this.Rotation);
+
+            bool flag = false;
+
+            if (!GenGrid.Impassable(intVec3_2) && !GenGrid.Impassable(intVec3_1))
+            {
+                float temperature1 = GridsUtility.GetTemperature(intVec3_1);
+                float temperature2 = GridsUtility.GetTemperature(intVec3_2);
+                float temperatureDiff = temperature1 - temperature2;
+                /*
+                //float energyLimit = (float)((double)this.compTempControl.props.energyPerSecond * temperatureDiff);
+
+                float num2 = (float)(1.0 - (double)temperatureDiff * (1.0 / 130.0));
+                float energyLimit = (float)((double)this.compTempControl.props.energyPerSecond * (double)num2 * 4.16666650772095);
+                 * 
+                12.0f * (double)num2 * 4.16666650772095);
+                 * 
+                energyLimit = Math.Abs(energyLimit);*/
+
+                //float energyLimit = 100.0f;
+
+                float energyLimit = Math.Abs (temperatureDiff *25.0f);
+
+                float maxEnergyLimit = 400.0f;
+
+                if (energyLimit > maxEnergyLimit)
+                {
+                    energyLimit = maxEnergyLimit;
+                }
+
+                Log.Message("Limit: " + energyLimit + " temperature1 " + temperature1 + " temperature2 " + temperature2);
+
+                if (temperature1 > temperature2)
+                {
+                    GenTemperature.TryControlTemperature(intVec3_1, -energyLimit, temperature2);
+                    GenTemperature.TryControlTemperature(intVec3_2, energyLimit, temperature1);
+                }
+                else
+                {
+                    GenTemperature.TryControlTemperature(intVec3_1, energyLimit, temperature2);
+                    GenTemperature.TryControlTemperature(intVec3_2, -energyLimit, temperature1);
+                }
+
+
+                /*
+                if (temperatureDiff > 0)
+                {
+                    //flag = GenTemperature.TryControlTemperature(intVec3_1, energyLimit, this.compTempControl.targetTemperature);
+                    flag = GenTemperature.TryControlTemperature(intVec3_1, energyLimit, temperature2);
+                    if (flag)
+                    {
+                        GenTemperature.PushHeat(intVec3_2, (float)(-(double)energyLimit));
+                    }
+                }
+                else
+                {
+                    //energyLimit = -energyLimit;
+                    flag = GenTemperature.TryControlTemperature(intVec3_2, energyLimit, temperature1);
+                    //flag = GenTemperature.TryControlTemperature(intVec3_2, energyLimit, this.compTempControl.targetTemperature);
+                    if (flag)
+                    {
+                        GenTemperature.PushHeat(intVec3_1, (float)(-(double)energyLimit));
+                    }
+                }*/
+
+            }
+
+        }
+
+        /* public override void TickRare()
         {
             if (!this.compPowerTrader.PowerOn)
             {
@@ -62,7 +138,7 @@ namespace Enhanced_Defence.Temperature
 
             }
 
-        }
+        }*/
 
         public override IEnumerable<Command> GetCommands()
         {
